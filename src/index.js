2db1b7ec8d1df79a83e1f55de4c489f2f058ffb5
Ritikshroff <109219868+Ritikshroff@users.noreply.github.com>
2024-03-05 13:16:35 +0530

    mongoDB connection setup

---
+++ src/index.js
@@ -0,0 +1,58 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+import { app } from "./app.js";
+
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+const app = express();
+
+// using iffi functon
+(async () => {
+    try {
+        await mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("err: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listning on port ${process.env.PORT}`);
+        })
+
+
+    } catch (error) {
+        console.error("error", error )
+        throw err;
+    }
+})()
+
+*/
