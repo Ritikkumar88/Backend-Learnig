c7b6c0e510f9f0a0cba3e732ad6e30dc7cc6f931
Ritikshroff <109219868+Ritikshroff@users.noreply.github.com>
2024-03-05 14:07:39 +0530

    utils code done

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
-
+// databse connection
 const connectDB = async () => {
     try {
         const connectionInstence = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,27 @@
+
+// wrapping the batabase connection function using promises//
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req , res, next)).catch((Err) => next(Err))
+    } 
+}
+
+export {asyncHandler};
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}   ** how of Higher order function
+
+
+// wrapping the batabase connection function using async await try catch//
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
